3f256206e160325e65038bf3bb56f4f7d71602f3
sahil <sahil@fullcreatives-MacBook-Pro.local>
2021-09-16 19:51:29 +0530

    string indexOf

--- Strings/indexOf.js
+++ Strings/indexOf.js
@@ -2,11 +2,15 @@ const str = "Hello World Hello";
 
 console.log(str.indexOf('H'));
 console.log(str.indexOf("Hello"));
-console.log(str.indexOf("World"))
+console.log(str.indexOf("World"));
+console.log(str.indexOf("Helloo"));
+console.log(str.indexOf("Hello o"))
 
 /*
 Output:
 0
 0
 6
+-1
+-1
  */
