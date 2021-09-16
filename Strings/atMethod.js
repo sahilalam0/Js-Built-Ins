3427c39d5240ba1245836370cb380d92241f9490
sahil <sahil@fullcreatives-MacBook-Pro.local>
2021-09-16 18:59:04 +0530

    string at method

---
+++ Strings/atMethod.js
@@ -0,0 +1,21 @@
+const str = "Hello World";
+
+//for loop to log characters at each index of the string str
+for (let ind = 0; ind < str.length; ind++) {
+    console.log(str.charAt(ind));
+}
+
+/*
+Output:
+H
+e
+l
+l
+o
+
+W
+o
+r
+l
+d
+*/
