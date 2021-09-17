0b6355263a53205e8adabe36294e500ff6adc0f6
sahil <sahil@fullcreatives-MacBook-Pro.local>
2021-09-17 15:02:59 +0530

    string repeat method

---
+++ Strings/Methods/repeat.js
@@ -0,0 +1,10 @@
+const str = "Hello World!,";
+
+console.log(str.repeat(2));
+console.log(str.repeat(3));
+
+/**
+ Output:
+ Hello World!,Hello World!,
+ Hello World!,Hello World!,Hello World!,
+ */
