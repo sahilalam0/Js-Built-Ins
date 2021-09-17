a02fa92b22aae6c2f7d5761f56a66d1131b452ff
sahil <sahil@fullcreatives-MacBook-Pro.local>
2021-09-17 18:59:09 +0530

    string split.js

---
+++ Strings/Methods/split.js
@@ -0,0 +1,10 @@
+const str = "Hello World, How are you?";
+
+console.log(str.split(' '));
+console.log(str.split(','));
+
+/*
+ Output:
+ [ 'Hello', 'World,', 'How', 'are', 'you?' ]
+ [ 'Hello World', ' How are you?' ]
+*/
