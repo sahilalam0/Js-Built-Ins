6ebc3e50a12b4cd3947c4dfd4cfd5be7e97fa796
sahil <sahil.mansoor@anywhere.co>
2021-09-20 21:45:18 +0530

    array methods and properties

---
+++ Arrays/Properties/length.js
@@ -0,0 +1,20 @@
+const arr1 = [0, 1, "Sahil", {
+    a: 1
+}];
+const arr2 = [0, 1, 2];
+
+
+console.log(arr1);
+console.log("Type : " + typeof arr1, "Length : " + arr1.length);
+
+console.log(arr2);
+console.log("Type : " + typeof arr2, "Length : " + arr2.length);
+
+/*
+Output:
+[ 0, 1, 'Sahil', { a: 1 } ]
+Type : object Length : 4
+
+[ 0, 1, 2 ]
+Type : object Length : 3
+*/
