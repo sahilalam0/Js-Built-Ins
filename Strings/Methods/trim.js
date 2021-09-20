24ae80374d8dd9f5546eb28a643192a6e32985dc
sahil <sahil.mansoor@anywhere.co>
2021-09-20 14:51:49 +0530

    string trim methods

--- Strings/Methods/trim.js
+++ Strings/Methods/trim.js
@@ -4,16 +4,33 @@ const strings = [
     "\tHello World\n"
 ];
 
-strings.forEach((str) => {
-    console.log('Original : "' + str + '" , ' + 'Trimmed : "' + str.trim() + '"\n');
+strings.forEach((str, ind) => {
+    console.log("String " + (ind + 1) + ':');
+    console.log('Original : "' + str + '"');
+    console.log('Trimmed : "' + str.trim() + '"');
+    console.log('TrimmedStart : "' + str.trimStart() + '"');
+    console.log('TrimmedEnd : "' + str.trimEnd() + '"\n');
 });
 
 /*
 Output:
-Original : "   Hello World    " , Trimmed : "Hello World"
+String 1:
+Original : "   Hello World    "
+Trimmed : "Hello World"
+TrimmedStart : "Hello World    "
+TrimmedEnd : "   Hello World"
 
-Original : "Hello    World" , Trimmed : "Hello    World"
+String 2:
+Original : "Hello    World"
+Trimmed : "Hello    World"
+TrimmedStart : "Hello    World"
+TrimmedEnd : "Hello    World"
 
+String 3:
 Original : "    Hello World
-" , Trimmed : "Hello World"
+"
+Trimmed : "Hello World"
+TrimmedStart : "Hello World
+"
+TrimmedEnd : "  Hello World"
 */
